a79ead7da7c229689498c31a1002af4cc6a4cc9e
Tzitza <ervistzitza@gmail.com>
2025-08-15 19:31:18 +0200

    Update star_rating.dart

--- lib/widgets/star_rating.dart
+++ lib/widgets/star_rating.dart
@@ -1,4 +1,4 @@
-.import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 class StarRating extends StatelessWidget {
   final double rating;
@@ -34,3 +34,4 @@ class StarRating extends StatelessWidget {
   }
 
 }
+
