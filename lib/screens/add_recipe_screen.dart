414a3261c4eb609d731f71f0c3649690afb8b084
Tzitza <ervistzitza@gmail.com>
2025-08-17 16:52:53 +0200

    Update add_recipe_screen.dart

--- lib/screens/add_recipe_screen.dart
+++ lib/screens/add_recipe_screen.dart
@@ -1,4 +1,4 @@
-.import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
@@ -239,3 +239,4 @@ class _AddRecipeScreenState extends State<AddRecipeScreen> {
   }
 
 }
+
