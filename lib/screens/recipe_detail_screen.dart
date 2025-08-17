127b8205258030878ecbaccebaa6425162d18bcd
Tzitza <ervistzitza@gmail.com>
2025-08-17 16:53:28 +0200

    Update recipe_detail_screen.dart

--- lib/screens/recipe_detail_screen.dart
+++ lib/screens/recipe_detail_screen.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+.import 'package:flutter/material.dart';
 import '../models/recipe.dart';
 import '../widgets/star_rating.dart';
 import 'dart:io';
@@ -182,4 +182,5 @@ class _InfoCard extends StatelessWidget {
       ),
     );
   }
+
 }
